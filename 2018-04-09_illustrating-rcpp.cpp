cc8e4f9ab726322c0d0d8ae0aa96e7d68a0ec144
pgurazada <pavankumar.gurazada@gmail.com>
2018-04-10 10:52:35 +0400

    initial files for may 2018 talk

---
+++ 2018-04-09_illustrating-rcpp.cpp
@@ -0,0 +1,34 @@
+#include <Rcpp.h>
+
+using namespace Rcpp;
+
+// Enable C++11 via this plugin (Rcpp 0.10.3 or later)
+// [[Rcpp::plugins("cpp11")]]
+
+// Example 1: Illustrating the data types that map R objects to Rcpp objects
+
+// [[Rcpp::export]]
+double calculate_sum(NumericVector x) {
+  double total = 0;
+  for (NumericVector::iterator it = x.begin(); it != x.end(); ++it) {
+    total += *it;
+  }
+  return total;
+}
+
+// Example 2: Illustrating how STL and C++11 features can be used
+
+// [[Rcpp::export]]
+int simple_product(std::vector<int> vec) {
+  auto prod = 1;
+  for (auto &x : vec) {       
+    prod *= x;              
+  }
+  return prod;
+}
+
+
+
+/*** R
+
+*/
