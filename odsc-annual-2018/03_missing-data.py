7deefe3090be62e33b8e181d06d01234270d1e91
Pavan Gurazada <pavankumar.gurazada@gmail.com>
2018-11-15 15:25:24 +0400

    over the board class on missing data information

---
+++ odsc-annual-2018/03_missing-data.py
@@ -0,0 +1,44 @@
+import warnings
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import missingno as msno
+
+sns.set_context('talk')
+sns.set_style('ticks')
+plt.rcParams['figure.figsize'] = (12, 6)
+
+
+warnings.filterwarnings(action='ignore', category=FutureWarning)
+
+
+class MissingDataInformer:
+
+    def __init__(self, n=10):
+        self.max_features = n
+    
+    def compute_value_counts(self, data_df):
+        return data_df.isnull().sum().sort_values(ascending=False)
+
+    def compute_value_perc(self, data_df):
+        return data_df.isnull().sum().sort_values(ascending=False) * 100/data_df.shape[0]
+
+    def cols_with_missings(self, data_df):
+        return [col for col in data_df.columns if data_df[col].isnull().any()]
+        
+
+if __name__ == '__main__':
+
+    ufo_df = pd.read_feather('data/ufo.feather')
+    print(ufo_df.shape)
+
+    mdi = MissingDataInformer()
+
+    print(mdi.compute_value_counts(ufo_df))
+    print(mdi.compute_value_perc(ufo_df))
+    print(mdi.cols_with_missings(ufo_df))
+
+    msno.heatmap(ufo_df, figsize=(20, 20))
+
+    plt.show()
+
