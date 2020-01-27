d42fcf3dc424d9e31546544f44d639b250cd0da7
Pavan <pavan.g@constems-ai.com>
2020-01-27 09:21:05 +0000

    unicolor wordclouds

--- abstract-word-clouds.py
+++ abstract-word-clouds.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 import numpy as np
 
@@ -5,20 +7,26 @@ import matplotlib.pyplot as plt
 
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
+
+def grey_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % random.randint(49, 51)
+
 papers_data_df = pd.read_csv('papers-list.csv')
 stopwords = set(STOPWORDS)
 
 for additional_word in ['data', 'visualization', 'visualizations', 'visualize']:
     stopwords.add(additional_word)
 
-papers_data_df = papers_data_df.loc[papers_data_df.Year >= 2009]
+papers_data_df = papers_data_df.loc[papers_data_df.Year >= 2009, :]
 
 
-for year in np.unique(papers_data_df.Year):
+for year in [2009, 2018]:
     subset_data_df = papers_data_df.loc[papers_data_df.Year == year]
     abstracts = '\n'.join(subset_data_df.Abstract)
     
     wc = WordCloud(background_color="white",
+                   collocations=False,
                    width=600,
                    height=400,
                    max_words=50, 
@@ -28,16 +36,10 @@ for year in np.unique(papers_data_df.Year):
     
     wc.generate(abstracts.lower())
     
-    plt.imshow(wc, interpolation='bilinear')
+    plt.imshow(wc.recolor(color_func=grey_color_func, random_state=20130810),
+               interpolation='bilinear')
     plt.axis("off")
     plt.show()
     
     wc.to_file(str(year)+'.jpg')
-    
-    
-    
-    
-    
-
-
 
